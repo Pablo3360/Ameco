220274ebcc98503f2114b52511ec05762929b08d
Pablo Gomez <pablogomez3360@gmail.com>
2023-02-08 15:52:27 -0300

    Add grafico titularesPorEdades

--- client/src/actions/statisticsTitulares.js
+++ client/src/actions/statisticsTitulares.js
@@ -14,3 +14,18 @@ export function getTitularesPorSexo() {
     if(response){ dispatch(getTitularesPorSexoResponse(response)) };
   }
 };
+
+export function getTitularesPorEdadesResponse(titularesPorEdades){
+  return {
+    type: 'TITULARES_EDADES',
+    payload: titularesPorEdades
+  }
+};
+
+export function getTitularesPorEdades() {
+  return async function(dispatch) {
+    const url = '/statistics/titularesPorEdades';
+    const response = await fetchData({ url }, dispatch);
+    if(response){ dispatch(getTitularesPorEdadesResponse(response)) };
+  }
+};
